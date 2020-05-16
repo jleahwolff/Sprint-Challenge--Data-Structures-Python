a580ce1a86f85f806fd3023c939890830d7395d9
Jessica Wolff <jleahwolff@gmail.com>
2020-05-16 14:00:15 -0500

    Done with ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,51 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.oldest = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) != self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage.pop(self.oldest)
+            self.storage.insert(self.oldest, item)
+
+            if self.oldest == self.capacity - 1:
+                self.oldest = 0
+            else:
+                self.oldest += 1
+            
 
     def get(self):
-        pass
+        return self.storage
+
+
+
+# A ring buffer is a non-growable buffer with a fixed size. When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element. This kind of data structure is very useful for use cases such as storing logs and history information, where you typically want to store information up until it reaches a certain age, after which you don't care about it anymore and don't mind seeing it overwritten by newer data.
+
+# Implement this behavior in the RingBuffer class. RingBuffer has two methods, `append` and `get`. The `append` method adds the given element to the buffer. The `get` method returns all of the elements in the buffer in a list in their given order. It should not return any `None` values in the list even if they are present in the ring buffer.
+
+# For example:
+
+# ```python
+# buffer = RingBuffer(3)
+
+# buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   # should return ['d', 'e', 'f']
+# ```
